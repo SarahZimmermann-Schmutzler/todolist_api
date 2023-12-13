10055b548b16c724cd6bb7d10f0986fc84a6f6cc
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-12-13 11:44:02 +0100

    user serializer

--- todo/serializers.py
+++ todo/serializers.py
@@ -2,8 +2,13 @@ from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 from .models import Todo
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
 
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    user = UserSerializer()
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'description', 'created_at']
+        fields = ['id', 'title', 'description', 'created_at', 'user']
