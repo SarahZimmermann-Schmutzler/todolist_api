6462dfc204ba3c2074c6e6a015f27d5212b387b2
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-12-13 15:16:15 +0100

    create funktion POST

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render
 from django.contrib.auth.models import Group, User
 from rest_framework import permissions, viewsets
@@ -7,8 +9,15 @@ from .serializers import TodoSerializer
 # Create your views here.
 class TodoViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows todos to be viewed or edited.
     """
     queryset = Todo.objects.all().order_by('-created_at')
     serializer_class = TodoSerializer
     # permission_classes = [permissions.IsAuthenticated]
+
+    def create(self, request):
+        todo = Todo.objects.create(title = request.POST.get('title', ''), 
+                                   description = request.POST.get('description', ''), 
+                                   user = request.user)
+        serialized_obj = serializers.serialize('json', [todo,])
+        return HttpResponse(serialized_obj, content_type='application/json')
