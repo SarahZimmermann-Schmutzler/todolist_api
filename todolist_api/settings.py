6a86481012eee2f13210c2ad65fcfde875367654
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-12-13 10:39:59 +0100

    new requirements

--- todolist_api/settings.py
+++ todolist_api/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-=@+^@@7wmung^l=is7tcfz!o&^d^u#)(zs)l%s&f7xf!1qn2cj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['SarahLucia.pythonanywhere.com']
+ALLOWED_HOSTS = [
+    'SarahLucia.pythonanywhere.com',
+    '127.0.0.1'
+    ]
 
 
 # Application definition
